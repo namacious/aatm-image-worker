d2e766a0670823fd1d28ee1fe724c09a9e1d4769
Dan Javed <danjaved@Dans-MacBook-Pro.local>
2026-02-28 18:35:01 -0800

    Updated Conf

--- config.php
+++ config.php
@@ -1,27 +1,23 @@
 <?php
-
 return [
     'redis' => [
-        'host' => '127.0.0.1',
-        'port' => 6379,
-        'queue' => 'card-image',
+        'host' => getenv('REDIS_HOST') ?: '172.31.16.0',
+        'port' => (int)(getenv('REDIS_PORT') ?: 6379),
+        'queue' => getenv('REDIS_QUEUE') ?: 'card-image',
     ],
-
     'db' => [
-        'host' => '67.222.12.208',
-        'database' => 'dreamwarriordev_aatm_mock_db',
-        'username' => 'dreamwarriordev_aatm_mock_dbuser',
-        'password' => 'HV3ra5PHDQ.PqAk(',
+        'host'     => getenv('DB_HOST') ?: 'aatm-aurora-cluster.cluster-ctiiwqm0m04r.us-west-2.rds.amazonaws.com',
+        'database' => getenv('DB_DATABASE') ?: 'aatm_apidb26',
+        'username' => getenv('DB_USERNAME'),
+        'password' => getenv('DB_PASSWORD'),
     ],
-
     'r2' => [
-        'endpoint'  => 'https://9a431a3523d4434220cf99798ae9e66e.r2.cloudflarestorage.com',
-        'bucket'    => 'aatm-share-cards',
-        'accessKey' => 'cc8db09f8c1a5e450fd183460da95ae8',
-        'secretKey' => 'ed5f02dd508cd35f9da0bbe278576d200ef22a371398875fefdc387b4b3a8bf6',
-        'publicUrl' => 'https://card-cdn.americaatthemovies.com',
+        'endpoint'  => getenv('R2_ENDPOINT'),
+        'bucket'    => getenv('R2_BUCKET') ?: 'aatm-share-cards',
+        'accessKey' => getenv('R2_ACCESS_KEY'),
+        'secretKey' => getenv('R2_SECRET_KEY'),
+        'publicUrl' => getenv('R2_PUBLIC_URL') ?: 'https://card-cdn.americaatthemovies.com',
     ],
-
     'assets' => [
         'templates_dir' => __DIR__ . '/assets/templates'
     ],
